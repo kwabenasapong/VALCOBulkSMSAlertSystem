b64b487e0bf1391b93a2e6ad6377631c58477fac
Kwabena Sapong <kwabenasapong@gmail.com>
2023-06-21 22:43:58 +0000

    Solve the logout problem and add only admin register feature

--- VALCOBulkSMSAlertSystem/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ VALCOBulkSMSAlertSystem/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -22,7 +22,6 @@ using VALCOBulkSMSAlertSystem.Areas.Identity.Data;
 
 namespace VALCOBulkSMSAlertSystem.Areas.Identity.Pages.Account
 {
-    [AllowAnonymous]
     public class RegisterModel : PageModel
     {
         private readonly SignInManager<VALCOUser> _signInManager;
