d30763c9bcd8b459ab2c9e3d4311ddacf45eb24d
Kwabena Sapong <kwabenasapong@gmail.com>
2023-06-26 23:39:23 +0000

    disable secrets.json file

--- VALCOBulkSMSAlertSystem/Program.cs
+++ VALCOBulkSMSAlertSystem/Program.cs
@@ -60,10 +60,10 @@ using (var scope = app.Services.CreateScope())
     // Set password with the Secret Manager tool.
     // dotnet user-secrets set SeedUserPW <pw>
 
-    var testUserPw = builder.Configuration.GetValue<string>("SeedUserPW");
+    /*var testUserPw = builder.Configuration.GetValue<string>("SeedUserPW");*/
 
     // Changed to manage admin password in appsettings.json
-    /*var testUserPw = builder.Configuration["AdminPW:Password"];*/
+    var testUserPw = builder.Configuration["AdminPW:Password"];
 
     await SeedData.Initialize(services, testUserPw);
 }
